4d9ff3d728b50ed0b28e0ec16a24e6a2e0bea1ee
Julie Coleman <juliecoleman820@gmail.com>
2020-05-20 01:03:48 +0000

    Added all javascript

--- arrays.js
+++ arrays.js
@@ -3,17 +3,30 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const idx in items) {
+    console.log(`${items[idx] } ${idx}`);
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+
+  for (const idx in items) {
+    if (idx % 2 === 0) {
+      result.push(items[idx]);
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b);
+  const newSortedItems = sortedItems.slice(0, n);
+  newSortedItems.reverse();
+
+  console.log(newSortedItems)
 }
